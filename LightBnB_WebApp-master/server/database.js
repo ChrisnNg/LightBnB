2397db400b3efebc336c3a874364a5af0e265636
ChrisnNg <cristopherng@hotmail.com>
2019-09-19 18:51:06 +0000

    getUserWIthEmail & getUserWith Id return a user from the database. addUser implemented but does not automatically log in

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -16,16 +16,17 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+  console.log('getUserWithEmail function started', email);
+  const queryString = `
+  SELECT *
+  FROM users
+  WHERE email = $1;
+  `;
+  
+  return pool.query(queryString, [email])
+    .then(res => res.rows[0])
+    .catch(err => null);
+
 };
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -35,7 +36,16 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  
+  const queryString = `
+  SELECT *
+  FROM users
+  WHERE id = $1;
+  `;
+
+  return pool.query(queryString, [id])
+    .then(res => res.rows)
+    .catch(err => null);
 };
 exports.getUserWithId = getUserWithId;
 
@@ -46,10 +56,12 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  console.log(user);
+  const queryString = `
+    INSERT INTO users (name, email, password)
+    VALUES ($1, $2, $3) RETURNING *;
+  `;
+  return pool.query(queryString, [user.name, user.email, user.password]);
 };
 exports.addUser = addUser;
 
